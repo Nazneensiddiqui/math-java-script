98c4368dc7a61260b12ce34076da570b42c174b4
naneen <nazneenbanosiddiqui@gmail.com>
2024-08-23 18:46:20 +0530

    second commit

--- math.js
+++ math.js
@@ -54,20 +54,26 @@
 // document.getElementById("demo9").innerHTML = z
 
 // // random number between 0 (inclusive) and 1 (exclusive)
-// let h = Math.random();  //....0 or 1 batayega  point me value dega
-// h = math.random()*100; //.....1 se 100 ke bich ki value dega but point me dega
-// h = math.floor(Math.random()*100)  // ..point me value nahi dega
-// document.getElementById("demo10").innerHTML = h
+ //let h = Math.random();  //....0 or 1 batayega  point me value dega
+ let h = Math.random()*100; //.....1 se 100 ke bich ki value dega but point me dega
+//let h = Math.floor(Math.random()*100)  // ..point me value nahi dega
+ document.getElementById("demo10").innerHTML = h
+ console.log(h)
 
-//.....
+//.................................................................
 // function randomnum(min,max){
-//     return Math.floor(Math.random() * (max - min + 1) + min);
+//    return Math.floor(Math.random() * (max - min + 1) + min);
+// //     for(let i=30; i<=200; i++){
+// //         return Math.floor(Math.random() * (max - min + 1) + min);
+// // }
 // }
 // let b = randomnum(30,200)
-// //console.log(b);
+// console.log(b);
 // document.getElementById("demo11").innerHTML = b;
 
-// const arry = ["BMW" , 'FORD', 'KIA','TATA' , 'SUZUKI',"TOYOTA"]
+
+//......................................string..array.............................
+//const arry = ["BMW" , 'FORD', 'KIA','TATA' , 'SUZUKI',"TOYOTA"]
 
 // const strinarry = arry.toString();
 // console.log(strinarry)
@@ -82,31 +88,49 @@
 // unshift and push methods of array retrun the length of the update array
  //push adds element to the end of array and unshift  adds element to the start
 
+//  console.log(arry.unshift("neno"))
+// console.log(arry)
+// //stating add array value
+// console.log(arry.push("neno"))//stating add array value
+// console.log(arry)
+
+
  //pop and shift methods return the value that is deleted
  //pop methods deletes an element from the end
  //shift method deletes an element from the start.
 
+//  console.log(arry.shift())
+//  console.log(arry)
+  
+// console.log(arry.pop())
+// console.log(arry)
+
+
  //..................monday..........demo12........
 
-//  const array1 = ["BMW" , 'FORD', 'KIA','AUDI' , 'SUZUKI',"TOYOTA"]
-//  console.log(array1);
-// array1.sort();  //aphabeticaly solt karega accending order
-// console.log(array1);
-// array1.reverse();  // aphabeticaliy revers karega as a decending order
-// console.log(array1);
+ const array1 = ["BMW" , 'FORD', 'KIA','AUDI' , 'SUZUKI',"TOYOTA"]
+ console.log(array1);
+array1.sort();  //aphabeticaly solt karega accending order
+console.log(array1);
+array1.reverse();  // aphabeticaliy revers karega as a decending order
+console.log(array1);
 
-//  document.getElementById("demo11").innerHTML = array1;
+ document.getElementById("demo11").innerHTML = array1;
 
 const num = [40,500,100,340,0,7]
-num.sort();
+num.sort();//first number ke accoding deta he
 console.log(num);
-document.getElementById("demo11").innerHTML = num;
+document.getElementById("demo12").innerHTML = num;
+
+num.reverse();//first number ke decending~ deta he
+console.log(num);
+document.getElementById("demo13").innerHTML = num;
 //compare function (assending)
  let a = num.sort(function(a,b){return a-b});
  console.log(a);
- document.getElementById("demo11").innerHTML = a;
+ document.getElementById("demo14").innerHTML = a;
  //decending order
 
- let d = num.sort(function(a,b){return b-a});
- console.log(d);
-document.getElementById("demo11").innerHTML = d;
+ let  b = num.sort(function(a,b){return b-a});
+ console.log(b);
+document.getElementById("demo15").innerHTML = b;
--- string.js
+++ string.js
@@ -1,33 +1,33 @@
-// const num = [40,500,100,340,0,7]
-// console.log(num);
-// document.getElementById("demo10").innerHTML = num;
-// //compare function (assending)
-//   num.sort(function(a,b){return a-b});
-//  console.log(num);
-//  //document.getElementById("demo10").innerHTML = num;
-//  //decending order
+const num = [40,500,100,340,0,7]
+console.log(num);
+document.getElementById("demo10").innerHTML = num;
+//compare function (assending) order me likhne ke liye sort function ka use karte he
+  num.sort(function(a,b){return a-b});
+ console.log(num);
+ //document.getElementById("demo10").innerHTML = num;
+ //decending order
 
-//  num.sort(function(a,b){return b-a});
-//  console.log(num);
-// //document.getElementById("demo10").innerHTML = num;
+ num.sort(function(a,b){return b-a});
+ console.log(num);
+//document.getElementById("demo10").innerHTML = num;
 
 
-// //............string
-// let x = "jonh";
-// // x = "10";
-// let city = "Bhopal"
-// // x = 'my name is \'john doe\' and he is john deo\'s friend';
+// //............string...........................................................
+let x = "jonh";
+// x = "10";
+let city = "Bhopal"
+// x = 'my name is \'john doe\' and he is john deo\'s friend';
 
-// // x = "Hello"+ " " + "wolrd" + " " + "i am" + " " + "x" + "from" + " " + city ;
+// x = "Hello"+ " " + "wolrd" + " " + "i am" + " " + "x" + "from" + " " + city ;
 
-// x = `Hello World Iam ${x} from ${city}`;
-// let l = x.length;
-// console.log(l);
-// console.log(typeof x);  // data type pata karne ke liye
-// document.getElementById('demo11').innerHTML = x;
+x = `Hello World Iam ${x} from ${city}`;
+let l = x.length;
+console.log(l);
+console.log(typeof x);  // data type pata karne ke liye
+document.getElementById('demo11').innerHTML = x;
 
-// y = `Hi my name is Nazneen and i am 28 years old , from Bhopal and doing Full Stack Course marn`
-// document.getElementById('demo11').innerHTML = y
+y = `Hi my name is Nazneen and i am 28 years old , from Bhopal and doing Full Stack Course marn`
+document.getElementById('demo13').innerHTML = y
 
 
 //utf 16  charcode ka code batata he
@@ -37,21 +37,27 @@ understanding javascript!!!`;
 
 let a = "hello everyone how you doing";
 let newstring = a.slice(4,12);
+console.log(newstring);// output "o every0"
 newstring = a.slice(5);
+console.log(newstring);// output stating ke 5 word ko chodkar sab print kardega
+// everyone how you doing
 newstring = a.slice(-10 , -5)
-console.log(newstring);
+console.log(newstring);// output -me he iska matlab oposite print kare ge
+//" you doing " is me se  " you " print hoga
 
-console.log(a[2]);
-console.log(a.at[10]);
-console.log(a.charAt(8));
-console.log(a.charCodeAt(3));
-//a = "hello" + " " + "everyone";
+console.log(a[2]); //to print l
+console.log(a.at[10]);// undifine
+console.log(a.charAt(8));//to print  e
+console.log(a.charCodeAt(3));// print charcode 108
 
-let b = a.concat(" " , "everyone");
+//...............concat se string me.. add kiya jata he ....................
+c = "hello" + " " + "everyone";
+
+let b = c.concat(" " , "your word is finised");
 
 document.getElementById("demo12").innerHTML = b
 
-
+//..................................table......................
 function display(){
   let data = document.getElementById("head");
 data.style.backgroundColor = "pink";
